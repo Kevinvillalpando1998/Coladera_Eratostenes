977ffc37e88e77c7a4291d64d2a287ba677c050d
Kevinvillalpando1998 <37672520+Kevinvillalpando1998@users.noreply.github.com>
2018-03-24 00:47:27 -0500

    Add files via upload

---
+++ coladera_Erastotenes/coladera_Erastotenes/Coladera.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coladera_Erastotenes
+{
+    class Coladera
+    {
+        static string b;
+
+
+
+
+
+
+
+        static void realizar()
+
+
+
+        {
+
+
+
+            int a = 1001;
+
+
+
+            bool[] marcados = new bool[a + 1];
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 2; i <= Math.Sqrt(a); i++)
+
+            {
+
+                if (!marcados[i]) //desigancion numeros no primos
+
+                    for (int j = i * 2; j <= a; j += i)
+
+                        marcados[j] = true;
+
+            }
+
+
+
+            for (int i = 2; i < marcados.Length; i++)
+
+                if (marcados[i] == false)
+
+                    builder.Append(i + ", ");
+
+            builder.Length -= 2;
+
+            b = builder.ToString();
+
+        }
+
+        public void comenzar()
+
+        {
+
+            realizar();
+
+        }
+
+        public override string ToString()
+
+        {
+
+            return b;
+
+        }
+    }
+}
---
+++ coladera_Erastotenes/coladera_Erastotenes/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace coladera_Erastotenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtmostrar = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtmostrar
+            // 
+            this.txtmostrar.Location = new System.Drawing.Point(143, 112);
+            this.txtmostrar.Multiline = true;
+            this.txtmostrar.Name = "txtmostrar";
+            this.txtmostrar.Size = new System.Drawing.Size(501, 243);
+            this.txtmostrar.TabIndex = 0;
+            this.txtmostrar.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(458, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generar Primos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtmostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtmostrar;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
